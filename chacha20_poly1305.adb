cbfaa80363126562e22bcd5773f4c01bc85331e1
Robert Bendun <rob.bendun@gmail.com>
2023-05-11 17:03:11 +0200

    Started IO work

--- chacha20_poly1305.adb
+++ chacha20_poly1305.adb
@@ -455,7 +455,48 @@ procedure Chacha20_Poly1305 is
 		Poly1305_Mac_Test;
 		ChaCha20_Aead_Encrypt_Test;
 	end Tests;
+
+	procedure Usage is
+	begin
+		Put_Line("usage:");
+		Put_Line("  chacha20_poly1305 <AAD file> <key file> <nonce file> <in file> <out file>");
+	end Usage;
+
+
+	Additional_Auth_Data, Key, Nonce, Input, Output : Byte_Array_Access;
+	Tag : Unsigned_8x16;
 begin
 	Tests;
 
+	if Ada.Command_Line.Argument_Count /= 5 then
+		Usage;
+		Ada.Command_Line.Set_Exit_Status(Ada.Command_Line.Failure);
+		return;
+	end if;
+
+	Additional_Auth_Data := Read_Binary_File(Ada.Command_Line.Argument(1));
+	Key := Read_Binary_File(Ada.Command_Line.Argument(2));
+	Nonce := Read_Binary_File(Ada.Command_Line.Argument(3));
+	Input := Read_Binary_File(Ada.Command_Line.Argument(4));
+
+	Put_Line(Integer'Image(Additional_Auth_Data'Length));
+	Put_Line(Integer'Image(Key'Length));
+	Put_Line(Integer'Image(Nonce'Length));
+	Put_Line(Integer'Image(Input'Length));
+
+	ChaCha20_Aead_Encrypt(
+		Additional_Auth_Data.all,
+		Key_8(Key.all),
+		Nonce_8(Nonce.all),
+		Input.all,
+		Output,
+		Tag
+	);
+
+
+	Delete(Additional_Auth_Data);
+	Delete(Input);
+	Delete(Key);
+	Delete(Nonce);
+	Delete(Output);
 end Chacha20_Poly1305;
